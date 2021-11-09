2b4172428d3557ee11c832fda1d92b57a08cd6d9
ivan schlagheck <ivansch05@gmail.com>
2021-11-09 17:20:15 +0100

    it has no style but it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,10 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article = Article.new(
+    title: Faker::GreekPhilosophers.quote,
+    content: Faker::Quote.famous_last_words
+  )
+  article.save!
+end
